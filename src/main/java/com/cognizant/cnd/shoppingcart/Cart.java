85a9e7694f611b82f81c2670941b95663f252e7d
Padmashree Ashokkumar <padhu2192@gmail.com>
2021-04-24 16:22:34 -0400

    Implemented test cases for sub total

--- src/main/java/com/cognizant/cnd/shoppingcart/Cart.java
+++ src/main/java/com/cognizant/cnd/shoppingcart/Cart.java
@@ -1,16 +1,30 @@
 package com.cognizant.cnd.shoppingcart;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Cart {
     List<Item> lineItems = new ArrayList<>();
 
-    public void AddItem(Item item){
+    public void addItem(Item item){
         lineItems.add(item);
     }
 
+    public void addAllItems(Item... items){
+        lineItems.addAll(Arrays.asList(items));
+    }
+
     public int itemCount(){
-        return -2;
+        return lineItems.size();
+    }
+
+    public double getSubTotal(){
+        double sum = 0.0;
+        for (Item i: lineItems
+             ) {
+                sum += i.getPrice();
+        }
+        return sum;
     }
 }
--- src/test/java/com/cognizant/cnd/shoppingcart/CartTest.java
+++ src/test/java/com/cognizant/cnd/shoppingcart/CartTest.java
@@ -1,6 +1,5 @@
 package com.cognizant.cnd.shoppingcart;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +18,13 @@ public class CartTest {
     void testThatCartIsEmpty() {
         int expected = 0;
         assertEquals(expected, cart.itemCount());
+    }
+    @Test
+    void testSubTotal(){
+        double expected = 8.0;
+        Item myItem = new Item("Fruits",5.0);
+        Item myItem2 = new Item("Glue",3.0);
+        cart.addAllItems(myItem,myItem2);
+        assertEquals(expected,cart.getSubTotal());
     }
 }
